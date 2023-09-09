272ded2750aec071724172c6ef13ff5e303cfeee
Horror Proton <107091537+horror-proton@users.noreply.github.com>
2023-09-09 23:41:39 +0800

    Update lspconfig for clangd

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -171,87 +171,86 @@ lspconfig.lua_ls.setup {
     },
 }
 
-require("clangd_extensions").setup {
-    server = {
-        on_attach = attach,
-        capabilities = capabilities,
-        cmd = {
-            "clangd",
-            "--background-index",
-            "--clang-tidy",
-            "--enable-config",
-            "--fallback-style=llvm",
-            "--all-scopes-completion",
-            "--completion-style=detailed",
-            "--header-insertion=iwyu",
-            "--header-insertion-decorators",
-            "--pch-storage=memory",
-            "--cross-file-rename",
-        },
-        -- options to pass to nvim-lspconfig
-        -- i.e. the arguments to require("lspconfig").clangd.setup({})
+lspconfig['clangd'].setup {
+    on_attach = function()
+        require("clangd_extensions.inlay_hints").setup_autocmd()
+        require("clangd_extensions.inlay_hints").set_inlay_hints()
+    end,
+    capabilities = { offsetEncoding = 'utf-16' },
+    cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--enable-config",
+        "--fallback-style=llvm",
+        "--all-scopes-completion",
+        "--completion-style=detailed",
+        "--header-insertion=iwyu",
+        "--header-insertion-decorators",
+        "--pch-storage=memory",
+        "--cross-file-rename",
     },
-    extensions = {
-        -- defaults:
-        -- Automatically set inlay hints (type hints)
-        autoSetHints = true,
-        -- These apply to the default ClangdSetInlayHints command
-        inlay_hints = {
-            -- Only show inlay hints for the current line
-            only_current_line = false,
-            -- Event which triggers a refersh of the inlay hints.
-            -- You can make this "CursorMoved" or "CursorMoved,CursorMovedI" but
-            -- not that this may cause  higher CPU usage.
-            -- This option is only respected when only_current_line and
-            -- autoSetHints both are true.
-            only_current_line_autocmd = "CursorHold",
-            -- whether to show parameter hints with the inlay hints or not
-            show_parameter_hints = true,
-            -- prefix for parameter hints
-            parameter_hints_prefix = "<- ",
-            -- prefix for all the other hints (type, chaining)
-            other_hints_prefix = "=> ",
-            -- whether to align to the length of the longest line in the file
-            max_len_align = false,
-            -- padding from the left if max_len_align is true
-            max_len_align_padding = 1,
-            -- whether to align to the extreme right or not
-            right_align = false,
-            -- padding from the right if right_align is true
-            right_align_padding = 7,
-            -- The color of the hints
-            highlight = "Comment",
-            -- The highlight group priority for extmark
-            priority = 100,
+}
+require("clangd_extensions").setup {
+    -- defaults:
+    -- Automatically set inlay hints (type hints)
+    autoSetHints = true,
+    -- These apply to the default ClangdSetInlayHints command
+    inlay_hints = {
+        -- Only show inlay hints for the current line
+        only_current_line = false,
+        -- Event which triggers a refersh of the inlay hints.
+        -- You can make this "CursorMoved" or "CursorMoved,CursorMovedI" but
+        -- not that this may cause  higher CPU usage.
+        -- This option is only respected when only_current_line and
+        -- autoSetHints both are true.
+        only_current_line_autocmd = "CursorHold",
+        -- whether to show parameter hints with the inlay hints or not
+        show_parameter_hints = true,
+        -- prefix for parameter hints
+        parameter_hints_prefix = "<- ",
+        -- prefix for all the other hints (type, chaining)
+        other_hints_prefix = "=> ",
+        -- whether to align to the length of the longest line in the file
+        max_len_align = false,
+        -- padding from the left if max_len_align is true
+        max_len_align_padding = 1,
+        -- whether to align to the extreme right or not
+        right_align = false,
+        -- padding from the right if right_align is true
+        right_align_padding = 7,
+        -- The color of the hints
+        highlight = "Comment",
+        -- The highlight group priority for extmark
+        priority = 100,
+    },
+    ast = {
+        role_icons = {
+            type = "",
+            declaration = "",
+            expression = "",
+            specifier = "",
+            statement = "",
+            ["template argument"] = "",
         },
-        ast = {
-            role_icons = {
-                type = "",
-                declaration = "",
-                expression = "",
-                specifier = "",
-                statement = "",
-                ["template argument"] = "",
-            },
-            kind_icons = {
-                Compound = "",
-                Recovery = "",
-                TranslationUnit = "",
-                PackExpansion = "",
-                TemplateTypeParm = "",
-                TemplateTemplateParm = "",
-                TemplateParamObject = "",
-            },
-            highlights = {
-                detail = "Comment",
-            },
+        kind_icons = {
+            Compound = "",
+            Recovery = "",
+            TranslationUnit = "",
+            PackExpansion = "",
+            TemplateTypeParm = "",
+            TemplateTemplateParm = "",
+            TemplateParamObject = "",
         },
-        memory_usage = {
-            border = "none",
+        highlights = {
+            detail = "Comment",
         },
-        symbol_info = {
-            border = "none",
-        },
+    },
+    memory_usage = {
+        border = "none",
+    },
+    symbol_info = {
+        border = "none",
     },
 }
 
