da3d945d64c7f7700cfdbceaf09873c7dfb6ac80
Horror Proton <107091537+horror-proton@users.noreply.github.com>
2024-04-30 11:13:15 +0800

    Update hyprland socket path for Hyprland/pull/5788

--- config/eww/scripts/hyprland.py
+++ config/eww/scripts/hyprland.py
@@ -5,10 +5,15 @@ INSTANCE_SIGNATURE = _os.getenv('HYPRLAND_INSTANCE_SIGNATURE')
 if INSTANCE_SIGNATURE is None:
     exit(1)
 
+_dir = (_os.getenv('XDG_RUNTIME_DIR')
+        or '/run/user/1000') + '/hypr/' + INSTANCE_SIGNATURE
+if (not _os.path.exists(_dir)):
+    _dir = '/tmp/hypr/' + INSTANCE_SIGNATURE
+
 
 def query(arg: bytes) -> bytes:
     with _socket.socket(_socket.AF_UNIX, _socket.SOCK_STREAM) as s:
-        s.connect('/tmp/hypr/' + INSTANCE_SIGNATURE + '/.socket.sock')
+        s.connect(_dir + '/.socket.sock')
         s.sendall(arg)
         data: bytes = b''
         while True:
@@ -22,5 +27,5 @@ def query(arg: bytes) -> bytes:
 
 def socket2(s=_socket.socket(_socket.AF_UNIX,
                              _socket.SOCK_STREAM)) -> _socket.socket:
-    s.connect('/tmp/hypr/' + INSTANCE_SIGNATURE + '/.socket2.sock')
+    s.connect(_dir + '/.socket2.sock')
     return s
