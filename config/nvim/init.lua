6cfcc6278acd9f382e17da7340dfbac15050b938
Horror Proton <107091537+horror-proton@users.noreply.github.com>
2025-01-31 23:48:40 +0800

    Use vim.diagnostic.config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -49,11 +49,20 @@ kmap("i", { "<M-S-k>", "<M-S-Up>" }, "<esc><cmd>m .-2<cr>==gi", { desc = "Move u
 kmap("v", { "<M-S-j>", "<M-S-Down>" }, ":m '>+1<cr>gv=gv", { desc = "Move down" })
 kmap("v", { "<M-S-k>", "<M-S-Up>" }, ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
-
-for type, icon in pairs({ Error = "►", Warn = "⚠", Hint = "", Info = "", }) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+vim.diagnostic.config({
+    virtual_text = true,
+    virtual_line = true,
+    severity_sort = true,
+    update_in_insert = true,
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "!►",
+            [vim.diagnostic.severity.WARN] = "⚠",
+            [vim.diagnostic.severity.HINT] = "",
+            [vim.diagnostic.severity.INFO] = "",
+        }
+    }
+})
 
 require('tmplog')
 require('desktop_notify')
